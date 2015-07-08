cec4609a64df1b71501e392d18afcffd72a477b8
Yuno <dianka.agafonova@gmail.com>
2015-07-08 21:08:00 +0300

    Create login-logout for user

--- university/urls.py
+++ university/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="home_page.html")),
+    url(r'^$', TemplateView.as_view(template_name="home_page.html"),
+        name="home_page"),
+    url(r'^login/$', 'django.contrib.auth.views.login', name="login"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': "home_page"}, name="logout"),
+    url(r'^accounts/profile/', RedirectView.as_view(pattern_name="home_page")),
     url(r'^admin/', include(admin.site.urls)),
 ]
