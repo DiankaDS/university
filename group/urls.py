8e8d97c3b9a50312a13e915e43c3662f689b7dc7
Yuno <dianka.agafonova@gmail.com>
2015-07-11 16:36:42 +0300

    Create html page with information about students in group

--- group/urls.py
+++ group/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from group.views import GroupsListView
+from group.views import GroupsListView, GroupsInfoView
 
 urlpatterns = patterns(
     '',
     url(r'^$', login_required(GroupsListView.as_view()), name="groups"),
+    url(r'^(?P<pk>\d+)/$', login_required(GroupsInfoView.as_view()),
+        name="info")
 )
-
--- group/views.py
+++ group/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from group.models import Group
 
 class GroupsListView(ListView):
     model = Group
     template_name = "groups_list.html"
+
+class GroupsInfoView(DetailView):
+    model = Group
+    template_name = "groups_info.html"
