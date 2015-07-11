45d2b27b182557fbada9b98476c7c0fb7b30e685
Yuno <dianka.agafonova@gmail.com>
2015-07-11 17:20:12 +0300

    Create update-form for groups

--- group/urls.py
+++ group/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from group.views import GroupsListView, GroupsInfoView
+from group.views import GroupsListView, GroupsInfoView, GroupUpdateView
 
 urlpatterns = patterns(
     '',
     url(r'^$', login_required(GroupsListView.as_view()), name="groups"),
     url(r'^(?P<pk>\d+)/$', login_required(GroupsInfoView.as_view()),
-        name="info")
+        name="info"),
+    url(r'(?P<pk>\d+)/update/$', login_required(GroupUpdateView.as_view()),
+        name="update"),
 )
--- group/views.py
+++ group/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.core.urlresolvers import reverse
+from django.views.generic import ListView, DetailView, UpdateView
 from group.models import Group
+
 
 class GroupsListView(ListView):
     model = Group
     template_name = "groups_list.html"
 
+
 class GroupsInfoView(DetailView):
     model = Group
     template_name = "groups_info.html"
+
+
+class GroupUpdateView(UpdateView):
+    model = Group
+    template_name = "update_group.html"
+
+    def get_success_url(self):
+        return reverse("groups:groups")
