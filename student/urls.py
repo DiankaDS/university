ae3902f859de408f29a73dd52585a22c82eaf174
Yuno <dianka.agafonova@gmail.com>
2015-07-11 18:47:47 +0300

    Create update-form for students

--- student/urls.py
+++ student/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from student.views import StudentsListView, StudentsInfoView, StudentCreateView
+from student.views import StudentsListView, StudentsInfoView, StudentCreateView, \
+    StudentUpdateView
 
 urlpatterns = patterns(
     '',
     url(r'^$', login_required(StudentsListView.as_view()), name="students"),
     url(r'^(?P<pk>\d+)/$', login_required(StudentsInfoView.as_view()),
         name="info"),
-    url(r'create_student$', login_required(StudentCreateView.as_view()),
+    url(r'create_student/$', login_required(StudentCreateView.as_view()),
         name="create"),
+    url(r'^(?P<pk>\d+)/update/$', login_required(StudentUpdateView.as_view()),
+        name="update"),
 )
--- student/views.py
+++ student/views.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from student.models import Student
 
 
@@ -16,6 +16,14 @@ class StudentsInfoView(DetailView):
 class StudentCreateView(CreateView):
     model = Student
     template_name = "create_student.html"
+
+    def get_success_url(self):
+        return reverse("students:students")
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    template_name = "update_student.html"
 
     def get_success_url(self):
         return reverse("students:students")
