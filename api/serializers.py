1ac161e462697d6dd14b7136238e59009e4709d1
Yuno <dianka.agafonova@gmail.com>
2015-07-17 15:11:03 +0300

    Add information about elder in Groups List Api

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from student.models import Student
 from group.models import Group
 
+
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
@@ -15,7 +16,21 @@ class StudentSerializer(serializers.ModelSerializer):
         )
 
 
+class ElderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = (
+            'first_name',
+            'last_name',
+            'middle_name',
+            'year_of_birth',
+            'student_id',
+        )
+
+
 class GroupSerializer(serializers.ModelSerializer):
+    elder = ElderSerializer()
+
     class Meta:
         model = Group
         fields = (
