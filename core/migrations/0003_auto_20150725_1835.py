441be7bbd505fb9ce90fe78174a4bdc99726a90f
Yuno <dianka.agafonova@gmail.com>
2015-07-27 00:09:51 +0300

    Update forms

---
+++ core/migrations/0003_auto_20150725_1835.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0002_userprofile_profile_image'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='userprofile',
+            name='profile_image',
+            field=models.ImageField(null=True, upload_to='img', blank=True),
+            preserve_default=True,
+        ),
+    ]
